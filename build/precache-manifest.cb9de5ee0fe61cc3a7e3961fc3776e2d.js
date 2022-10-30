4ea897bf443b88ae82526fc33492a0063c892b2d
ashutosh3027 <iit2020072@iiita.ac.in>
2022-10-30 16:58:33 +0530

    added build folder

---
+++ build/precache-manifest.cb9de5ee0fe61cc3a7e3961fc3776e2d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbc4430a11aeb718e9cc",
+    "url": "/static/css/main.7be8c751.chunk.css"
+  },
+  {
+    "revision": "dbc4430a11aeb718e9cc",
+    "url": "/static/js/main.dbc4430a.chunk.js"
+  },
+  {
+    "revision": "08877b0740168d9a6b08",
+    "url": "/static/css/1.1817fc3b.chunk.css"
+  },
+  {
+    "revision": "08877b0740168d9a6b08",
+    "url": "/static/js/1.08877b07.chunk.js"
+  },
+  {
+    "revision": "2dd0b45b38504d0bd523",
+    "url": "/static/css/2.cd8154ce.chunk.css"
+  },
+  {
+    "revision": "2dd0b45b38504d0bd523",
+    "url": "/static/js/2.2dd0b45b.chunk.js"
+  },
+  {
+    "revision": "2bc4d8955c5b1e8b5b69",
+    "url": "/static/js/3.2bc4d895.chunk.js"
+  },
+  {
+    "revision": "e978dc6a66012d5dfa9b",
+    "url": "/static/js/runtime~main.e978dc6a.js"
+  },
+  {
+    "revision": "7588e13138bea2fe559166fa78979aaa",
+    "url": "/static/media/aboutImg.7588e131.jpg"
+  },
+  {
+    "revision": "a5d58c878980ed7eede593cd94656c75",
+    "url": "/static/media/home_page.a5d58c87.svg"
+  },
+  {
+    "revision": "02b0026fa24226e2c28d6b7933548bd4",
+    "url": "/index.html"
+  }
+];
